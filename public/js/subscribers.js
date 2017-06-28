2c66ab57942541f9e256ed995c22148af7d1a3f3
microworlds <calebdavid20@gmail.com>
2017-06-28 23:51:51 +0100

    Added database

--- public/js/subscribers.js
+++ public/js/subscribers.js
@@ -2,18 +2,12 @@ var app = angular.module('app', []);
 
 app.controller('sub', ['$scope', '$http', function($scope, $http){
 
-		$scope.sub_show_me = false;
 		// Send message
 		$scope.subscriber = {};
 		
 		$scope.sending = function(){
 			$http.post('/users/subscribe', $scope.subscriber).success(function(info){
 				$scope.subscriber = null;
-				if (info.msg === "done") {
-					$scope.sub_show_me = true;
-				} else {
-					$scope.sub_show_me = false;
-				}
 			});
 		}
 
@@ -23,26 +17,13 @@ app.controller('sub', ['$scope', '$http', function($scope, $http){
 
 app.controller('con', ['$scope', '$http', function($scope, $http){
 
-		$scope.con_show_me = false;
 		// Send message
 		$scope.contact = {};
 		
 		$scope.send = function(){
 			$http.post('/users/contact', $scope.contact).success(function(info){
 				$scope.contact = null;
-				if (info.msg === "done") {
-					$scope.con_show_me = true;
-				} else {
-					$scope.con_show_me = false;
-				}
 			});
 		}
 
 	}]);
-
-
-"<div ng-show="sub_show_me" class="message is-success">
-  <div class="message-body">
-    <p>Congratulations, you have subscribed to our newsletter list.</p>
-  </div>
-</div>"
