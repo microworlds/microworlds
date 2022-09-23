8afa2a2cdfd5f9401c80539dbed977bc3aa5a036
Caleb <calebdavid20@gmail.com>
2022-09-23 12:59:42 +0100

    New site

--- README.md
+++
@@ -1,2 +0,0 @@
-# My website
-Welcome to my website. This is the same codebase that powers the website [Microworlds](http://microworlds.herokuapp.com).
--- routes/files.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.post('/download', function(req, res) {
-  re.end();
-});
-
-module.exports = router;
